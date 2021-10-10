4d6d44715d2181ec3947279dc933834ef17211a0
Ronak Kothari <rk.ronakjkothari@gmail.com>
2021-10-10 08:07:48 -0700

    woah dude

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,20 @@
 const sidebarLine = document.querySelectorAll('.sidebar-line');
 const friends = document.querySelectorAll('.navbar-friend');
+const dropdown = document.querySelectorAll('.dropdown')
 let activeSidebar = 0;
 
 function initialize(){
     initFriends();
+}
+
+function toggleDropdown(number){
+    if(dropdown[number].classList.contains('inactive-dropdown')){
+        dropdown[number].classList.add('active-dropdown')
+        dropdown[number].classList.remove('inactive-dropdown')
+    } else{
+        dropdown[number].classList.add('inactive-dropdown')
+        dropdown[number].classList.remove('active-dropdown')
+    }
 }
 
 function toggleSidebar(number){
