f13be3c9ccbf624cf2574998e1168769c3354a74
Ronak Kothari <rk.ronakjkothari@gmail.com>
2021-10-10 11:33:52 -0700

    Updates

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,10 +1,15 @@
 const sidebarLine = document.querySelectorAll('.sidebar-line');
 const friends = document.querySelectorAll('.navbar-friend');
 const dropdown = document.querySelectorAll('.dropdown')
+const rosterItem = document.querySelectorAll('.roster-item');
+
+const players = ["TrentBoult", "KaneWilliamson", "ViratKohli", "JaspritBumrah", "EoinMorgan", "ChrisGayle"]
+
 let activeSidebar = 0;
 
 function initialize(){
     initFriends();
+    initRoster();
 }
 
 function toggleDropdown(number){
@@ -14,6 +19,28 @@ function toggleDropdown(number){
     } else{
         dropdown[number].classList.add('inactive-dropdown')
         dropdown[number].classList.remove('active-dropdown')
+    }  
+}
+
+function initRoster(){
+    for(let i = 0; i < rosterItem.length; i++){
+        let playerName = players[Math.trunc(Math.random() * players.length)];
+        let newDiv = document.createElement('div')
+        let newDescDiv = document.createElement('div')
+        let newHeader = document.createElement('h5')
+        let newPara = document.createElement('p')
+
+        newDescDiv.classList.add('roster-desc')
+        newHeader.classList.add('roster-h5')
+        newPara.classList.add('roster-p')
+        newHeader.appendChild(document.createTextNode(playerName))
+        newPara.appendChild(document.createTextNode('Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil tempora fugiat, dolorem nisi similique dignissimos rem saepe minus repellat debitis ipsam quos! Distinctio, hic ipsa.'))
+        newDescDiv.appendChild(newHeader)
+        newDescDiv.appendChild(newPara)
+        newDiv.classList.add('roster-img');
+        newDiv.style.backgroundImage = "url('https://moneyball.insidesport.co/img/singleplayer/" + playerName + "1.jpg')";
+        newDiv.appendChild(newDescDiv)
+        rosterItem[i].appendChild(newDiv);
     }
 }
 
@@ -26,7 +53,6 @@ function toggleSidebar(number){
 function initFriends(){
     for(let i = 0; i < friends.length; i++){
         friends[i].src = "./icons/avatars/" + Math.trunc((Math.random() * 50) + 1) + ".svg"
-        console.log(friends[i].src)
     }
 }
 
