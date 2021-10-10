434d877518c85333b7d3562229e1814dac69236b
Ronak Kothari <rk.ronakjkothari@gmail.com>
2021-10-10 09:02:43 -0700

    Update algorithms.js

---
+++ js/algorithms.js
@@ -0,0 +1,18 @@
+function calculateScore(playerObj){
+    let score;
+    return score;
+}
+
+function calculateBatterScore(runs){
+    let score;
+    return score;
+}
+
+function calculateBowlerScore(wickets, economy, wides, noBalls, ballsBowled){
+    let score;
+    return score;
+}
+
+function calculateFielderScore(catches, runouts){
+
+}
