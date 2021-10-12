5c6e3cdf9d3734e8b76823f72260f40086145eff
ronakkothari123 <rk.ronakjkothari@gmail.com>
2021-10-11 22:01:32 -0700

    Login Theme Toggle
    Something

--- js/login.js
+++ js/login.js
@@ -2,6 +2,7 @@ const modals = document.querySelectorAll('.modal');
 
 let activeModal = 0;
 let passwordVisiblity = false;
+let darkTheme = false;
 
 function toggleModal(number){
     modals[activeModal].classList.remove('active-modal')
@@ -18,3 +19,28 @@ function togglePassword(){
     }
     passwordVisiblity = !passwordVisiblity;
 }
+
+function initialize(){
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        toggleTheme();
+    }
+}
+
+function toggleTheme(){
+    darkTheme = !darkTheme;
+    if(darkTheme){
+        document.querySelector(':root').style.setProperty('--text-color', 'white');
+        document.querySelector(':root').style.setProperty('--background-1', '#1b2028');
+        document.querySelector(':root').style.setProperty('--border-color', '#171a1f');
+        document.querySelector(':root').style.setProperty('--icon-brightness', 1);
+        document.getElementById('theme-toggler').src = "./icons/darkactive.svg"
+    } else {
+        document.querySelector(':root').style.setProperty('--text-color', 'black');
+        document.querySelector(':root').style.setProperty('--background-1', '#ffffff');
+        document.querySelector(':root').style.setProperty('--border-color', '#cccccc');
+        document.querySelector(':root').style.setProperty('--icon-brightness', 0);
+        document.getElementById('theme-toggler').src = "./icons/darkinactive.svg"
+    }
+}
+
+initialize()
