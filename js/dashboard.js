f368e09deb955c722ab87da5d201b51ad3fd87e3
Ronak Kothari <rk.ronakjkothari@gmail.com>
2021-10-21 11:45:43 -0700

    Nameplate Change

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,12 @@
 const profilePicture = document.querySelector("#pfp-image");
+const profileName = document.querySelector("#pfp-name")
 
 const colors = ["#E74C3C", "#8E44AD", "#3498DB", "#1ABC9C", "#2ECC71", "#F1C40F"]
-const specialChars = ["/", "$", "#", "%", "&", "~", "="]
 
 
 function randomProfile(){
-    profilePicture.innerHTML = specialChars[Math.trunc(Math.random() * specialChars.length)] + specialChars[Math.trunc(Math.random() * specialChars.length)];
+    const profileNames = profileName.innerHTML.split(" ")
+    profilePicture.innerHTML = profileNames[0][0].toLowerCase() + profileNames[profileNames.length - 1][0].toLowerCase();
     profilePicture.style.background = colors[Math.trunc(Math.random() * colors.length)]
 }
 
