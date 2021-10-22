5335a505a5ac81d8c2b2e8ea03ecce4c823a0b96
Ronak Kothari <rk.ronakjkothari@gmail.com>
2021-10-22 10:02:06 -0700

    Update Dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,13 @@
 const profilePicture = document.querySelector("#profile-pic");
-const profileName = document.querySelector("#profile-name")
+const profileName = document.querySelector("#profile-name");
+const bannerImage = document.querySelector("#banner-image");
+
+const filterButtons = document.querySelectorAll(".filter-para")
 
 const colors = ["#E74C3C", "#8E44AD", "#3498DB", "#1ABC9C", "#2ECC71", "#F1C40F"]
 
 const nameUpperCase = true;
+let activeFilter = 0;
 
 function randomProfile(){
     const profileNames = profileName.innerHTML.split(" ")
@@ -13,6 +17,16 @@ function randomProfile(){
         profilePicture.innerHTML = profileNames[0][0].toLowerCase() + profileNames[profileNames.length - 1][0].toLowerCase();
     }
     profilePicture.style.background = colors[Math.trunc(Math.random() * colors.length)]
+
+    bannerImage.src = "./images/people/person" + (Math.trunc(Math.random() * 5) + 1) + ".png"
+    console.log(bannerImage.src)
+}
+
+function toggleFilter(number){
+    filterButtons[activeFilter].classList.remove('active-filter')
+    filterButtons[number].classList.add('active-filter')
+    activeFilter = number;
 }
 
 randomProfile()
+toggleFilter(0);
