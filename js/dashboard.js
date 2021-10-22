5b6a6dc7711596e5e21715d75b5ef8ddf05a41d0
ronakkothari123 <rk.ronakjkothari@gmail.com>
2021-10-22 00:03:34 -0700

    Updated Dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,17 @@
-const profilePicture = document.querySelector("#pfp-image");
-const profileName = document.querySelector("#pfp-name")
+const profilePicture = document.querySelector("#profile-pic");
+const profileName = document.querySelector("#profile-name")
 
 const colors = ["#E74C3C", "#8E44AD", "#3498DB", "#1ABC9C", "#2ECC71", "#F1C40F"]
 
+const nameUpperCase = true;
 
 function randomProfile(){
     const profileNames = profileName.innerHTML.split(" ")
-    profilePicture.innerHTML = profileNames[0][0].toLowerCase() + profileNames[profileNames.length - 1][0].toLowerCase();
+    if(nameUpperCase){
+        profilePicture.innerHTML = profileNames[0][0].toUpperCase() + profileNames[profileNames.length - 1][0].toUpperCase();
+    } else {
+        profilePicture.innerHTML = profileNames[0][0].toLowerCase() + profileNames[profileNames.length - 1][0].toLowerCase();
+    }
     profilePicture.style.background = colors[Math.trunc(Math.random() * colors.length)]
 }
 
