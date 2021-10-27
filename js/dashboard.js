ebe4bc9e2c415c094b9b8926f84a88357585d56a
Ronak Kothari <rk.ronakjkothari@gmail.com>
2021-10-26 19:24:52 -0700

    Dashboard Minor Update

--- js/dashboard.js
+++ js/dashboard.js
@@ -5,7 +5,7 @@ const bannerImage = document.querySelector("#banner-image");
 const filterButtons = document.querySelectorAll(".filter-para")
 
 const colors = ["#E74C3C", "#8E44AD", "#3498DB", "#1ABC9C", "#2ECC71", "#F1C40F"];
-const types = ["result-item", "leagues-result", "upcoming-matches-result", "match-results-result", "players-result", "top-news-result"]
+const types = ["custom-feed", "leagues-result", "upcoming-matches-result", "match-results-result", "players-result", "top-news-result"]
 
 const nameUpperCase = true;
 let activeFilter = 0;
@@ -19,7 +19,7 @@ function randomProfile(){
     }
     profilePicture.style.background = colors[Math.trunc(Math.random() * colors.length)]
 
-    bannerImage.src = "../images/people/person" + (Math.trunc(Math.random() * 5) + 1) + ".png"
+    bannerImage.src = "../images/people/person" + (Math.trunc(Math.random() * 8) + 1) + ".png"
     console.log(bannerImage.src)
 }
 
@@ -28,7 +28,7 @@ function createNewsStory(type, imgLink, league, header, subheader, hearts, views
     
     let resultItem = document.createElement('div');
     resultItem.classList.add('result-item');
-    resultItem.classList.add(types[type])
+    resultItem.classList.add(types[type + 1])
 
     let image = document.createElement('img');
     image.src = imgLink;
@@ -81,7 +81,7 @@ function createNewsStory(type, imgLink, league, header, subheader, hearts, views
 }
 
 function toggleFilter(number){
-    const results = document.querySelectorAll('.' + types[0]);
+    const results = document.querySelectorAll('.result-item');
     const filteredResults = document.querySelectorAll('.' + types[number])
 
     filterButtons[activeFilter].classList.remove('active-filter')
@@ -100,19 +100,19 @@ function toggleFilter(number){
 
 function initialize(){
     randomProfile();
+    addNews()
     toggleFilter(0);  
 }
 
 function addNews(){
-    createNewsStory(3, "https://static.independent.co.uk/2021/10/25/18/newFile-1.jpg?width=982&height=726&auto=webp&quality=75", "t20wc", "Scotland Loses Badly", "Embarrasing Defeat", 234, 643);
-    createNewsStory(3, "https://c.ndtvimg.com/2021-10/hh03uuq8_pak_625x300_24_October_21.jpg", "t20wc", "India Loses 1st Time", "India vs. Pakistan", 2347, 9753);
-    createNewsStory(2, "https://media.istockphoto.com/photos/pakistan-and-india-two-flags-together-textile-cloth-fabric-texture-picture-id1093162828?k=20&m=1093162828&s=612x612&w=0&h=AWWrLn1rJX6HlviWqr1bt1k9u1wz6p_UOKt7KN60YQ0=", "t20wc", "India vs. Pakistan", "October 24th, 2021", 115, 5472)
-    createNewsStory(2, "https://www.dailynews.lk/sites/default/files/news/2021/05/24/01-Bangladesh%20%281%29.jpg", "t20wc", "Sri Lanka vs. Bangladesh", "October 24th, 2021", 234, 658);
-    createNewsStory(5, "https://www.wallpapertip.com/wmimgs/59-599019_rohit-sharma-hd-wallpapers-download-india-captain-rohit.jpg", "t20wc", "Rohit Sharma Captain", "Virat Kohli Steps Down", 4738, 2347)
-    createNewsStory(3, "https://emergingcricket.com/wp-content/uploads/2019/11/Netherlands-celebrate-run-out-of-Assadollah-Vala.jpg", "t20wc", "Netherlands Loses", "Embarrasing Loss", 569, 3245);
-    createNewsStory(5, "https://resources.pulse.icc-cricket.com/ICC/photo/2021/08/19/c5a6287c-ee68-4044-929a-143874ddeccb/Trophy-Tour_Poster_1920x1080-1-.png", 't20wc', 'T20 World Cup Begins', 'October 17th, 2021', 2490, 8234);
-    createNewsStory(1, "https://wallpapercave.com/wp/wp3156823.jpg", 'ipl', 'Chennai Wins IPL', 'IPL CSK x KKR', 9999, 9999);
+    createNewsStory(2, "https://static.independent.co.uk/2021/10/25/18/newFile-1.jpg?width=982&height=726&auto=webp&quality=75", "t20wc", "Scotland Loses Badly", "Embarrasing Defeat", 234, 643);
+    createNewsStory(2, "https://c.ndtvimg.com/2021-10/hh03uuq8_pak_625x300_24_October_21.jpg", "t20wc", "India Loses 1st Time", "India vs. Pakistan", 2347, 9753);
+    createNewsStory(4, "https://media.istockphoto.com/photos/pakistan-and-india-two-flags-together-textile-cloth-fabric-texture-picture-id1093162828?k=20&m=1093162828&s=612x612&w=0&h=AWWrLn1rJX6HlviWqr1bt1k9u1wz6p_UOKt7KN60YQ0=", "t20wc", "India vs. Pakistan", "October 24th, 2021", 115, 5472)
+    createNewsStory(4, "https://www.dailynews.lk/sites/default/files/news/2021/05/24/01-Bangladesh%20%281%29.jpg", "t20wc", "Sri Lanka vs. Bangladesh", "October 24th, 2021", 234, 658);
+    createNewsStory(4, "https://www.wallpapertip.com/wmimgs/59-599019_rohit-sharma-hd-wallpapers-download-india-captain-rohit.jpg", "t20wc", "Rohit Sharma Captain", "Virat Kohli Steps Down", 4738, 2347)
+    createNewsStory(2, "https://emergingcricket.com/wp-content/uploads/2019/11/Netherlands-celebrate-run-out-of-Assadollah-Vala.jpg", "t20wc", "Netherlands Loses", "Embarrasing Loss", 569, 3245);
+    createNewsStory(4, "https://resources.pulse.icc-cricket.com/ICC/photo/2021/08/19/c5a6287c-ee68-4044-929a-143874ddeccb/Trophy-Tour_Poster_1920x1080-1-.png", 't20wc', 'T20 World Cup Begins', 'October 17th, 2021', 2490, 8234);
+    createNewsStory(4, "https://wallpapercave.com/wp/wp3156823.jpg", 'ipl', 'Chennai Wins IPL', 'IPL CSK x KKR', 9999, 9999);
 }
 
 initialize()
-addNews()
